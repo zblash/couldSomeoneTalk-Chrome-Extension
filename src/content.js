e38fc9881fb1c72e8f5702bc402419dddc848149
Yusuf Can Celik <yusufcancelik@hotmail.com>
2020-07-29 01:42:56 +0300

    refactor

--- src/content.js
+++ src/content.js
@@ -6,56 +6,62 @@ function ParticipantsDOM() {
 
 ParticipantsDOM.prototype = {
   constructor: ParticipantsDOM,
-  getTextFromDOM: function () {
-    this.participants = this.participants.map((p) => p.innerHTML);
+  getTextFromDOM() {
+    this.participants = this.participants.map(p => p.innerHTML);
     return this;
   },
 
-  clearText: function () {
-    this.participants = this.participants.map((p) => p.replace("(me)", ""));
+  clearText() {
+    this.participants = this.participants.map(p => p.replace("(me)", ""));
     return this;
   },
 
-  send: function () {
+  send() {
     return this.participants;
-  },
+  }
 };
 
 function WriteToRoom(str) {
   this.textBtn = Array.from(
     document.getElementsByClassName("toolbox-button")
-  ).find((d) => d.ariaLabel === "Toggle chat window");
+  ).find(d => d.ariaLabel === "Toggle chat window");
   this.textBox;
   this.str = str;
 }
 
 WriteToRoom.prototype = {
   constructor: WriteToRoom,
-  click: function () {
+  click() {
     this.textBtn.click();
     return this;
   },
 
-  getTextBox: function () {
+  getTextBox() {
     this.textBox = document.getElementById("usermsg");
     return this;
   },
 
-  write: function () {
-    console.log(this);
+  write() {
     this.textBox.value = this.str;
-  },
+  }
 };
 
-chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
   if (msg.type === "GET_PARTICIPANTS_DOM") {
     const getParticipantsDOM = new ParticipantsDOM();
-    sendResponse(getParticipantsDOM.getTextFromDOM().clearText().send());
+    sendResponse(
+      getParticipantsDOM
+        .getTextFromDOM()
+        .clearText()
+        .send()
+    );
   }
 
   if (msg.type === "WRITE_ROOM") {
-    console.log(msg.str);
     const writeResult = new WriteToRoom(msg.str);
-    writeResult.click().getTextBox().write();
+    writeResult
+      .click()
+      .getTextBox()
+      .write();
   }
 });
